c94c4579f9bede521ad428751b9c7324ba1919ec
Brock Luker <brockluker@gmail.com>
2016-02-28 12:25:14 -0700

    Add fullname

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  # TO LEARN MORE ABOUT MODEL VALIDATIONS (http://www.guides.rubyonrails.org/active_record_validations.html)
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  validates :fullname, presence: true, length: {maximum: 50}       
 end
