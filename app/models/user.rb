4c502eabbb8a7ceb66e7e89e61b86c5caf606c22
Brock Luker <brockluker@gmail.com>
2016-02-28 14:25:10 -0700

    Added mandrill app

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   # TO LEARN MORE ABOUT MODEL VALIDATIONS (http://www.guides.rubyonrails.org/active_record_validations.html)
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable
          
   validates :fullname, presence: true, length: {maximum: 50}       
 end
