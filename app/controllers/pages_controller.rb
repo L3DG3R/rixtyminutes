5718b527937aeb62af95fbb5ded71098c6a5adf1
Brock Luker <brockluker@gmail.com>
2016-06-09 16:32:37 -0700

    Add search view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def home
     @projects = Project.all
   end
+  
+  def search
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
   resources :projects
   resources :photos
   
- # get '/search' => 'pages#search'
+  get '/search' => 'pages#search'
              
 end
